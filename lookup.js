53ec214a3c202373dfce096ab2bc65860d1aff23
Kyung-ha Lee <i_am@nulleekh.com>
2020-02-20 22:51:56 +0900

    [FEATURE] Add history lookup feature in lookup.js

--- lookup.js
+++ lookup.js
@@ -1,6 +1,39 @@
 const form = document.querySelector(".user-lookup-nickname-form"),
       input = document.querySelector(".user-lookup-nickname-input"),
-      uuid = document.querySelector(".user-lookup-uuid");
+      uuid = document.querySelector(".user-lookup-uuid"),
+      history = document.querySelector(".user-lookup-nickname-history");
+
+function showHistoryErr(){
+    history.innerHTML = 'ERR: Check console for more information.';
+}
+
+function showHistory(content) {
+    history.innerHTML = `${content}`;
+}
+
+function lookupNameHistory(uuid) {
+    fetch(
+        `https://api.mojang.com/user/profiles/${uuid}/names`
+    )
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(json) {
+            let checkedHistory = JSON.stringify(json);
+            checkedHistory = checkedHistory.replace('[{"', '');
+            checkedHistory = checkedHistory.replace('}]', '');
+            checkedHistory = checkedHistory.replace(/"},{"/gi, '<br>');
+            checkedHistory = checkedHistory.replace(/},{"/gi, '<br>');
+            checkedHistory = checkedHistory.replace(/":"/gi, ': ');
+            checkedHistory = checkedHistory.replace(/","/gi, ' | ');
+            checkedHistory = checkedHistory.replace(/":/gi, ': ');
+            showHistory(checkedHistory);
+        })
+        .catch(function (err) {
+            console.log(err);
+            showHistoryErr();
+        });
+}
 
 function showUuidErr() {
     uuid.innerHTML = 'ERR: Check console for more information.';
@@ -20,6 +53,7 @@ function lookup(nickname) {
         .then(function(json) {
             const checkedUuid = json['id'];
             showUuid(checkedUuid);
+            lookupNameHistory(checkedUuid);
         })
         .catch(function (err) {
             console.log(err);
